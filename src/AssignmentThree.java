8febe5d4e137e8e71915eaf1929fc69e4933dc81
Tyypos <tyyposofficial@gmail.com>
2019-03-19 20:28:57 -0700

    jared: added class stubs for Card, Hand and Deck

--- src/AssignmentThree.java
+++ src/AssignmentThree.java
@@ -10,7 +10,32 @@ public class AssignmentThree
 
    public static void main(String[] args)
    {
-      System.out.println("Hello, Team 6");
+      System.out.println("Hello, Team 6!");
    }
 
+}
+
+/*
+ * A class that represents a playing card
+ */
+class Card
+{
+   // empty
+}
+
+/*
+ * A class that represents the cards held by a single player
+ */
+class Hand
+{
+   // empty
+}
+
+/*
+ * A class that represents the source of the cards for dealing and,
+ * as the game progresses, the place from which players can receive new cards
+ */
+class Deck
+{
+   // empty
 }
