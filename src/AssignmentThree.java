41aac99863e0fbd657073da67741db8a411a19ae
Tyypos <tyyposofficial@gmail.com>
2019-03-19 20:12:00 -0700

    jared: initial commit of java class

---
+++ src/AssignmentThree.java
@@ -0,0 +1,16 @@
+/**
+ * This application simulates a card game...
+ * 
+ * @author Team 6: Jared Cheney, Andrew Meraz, Mina Mansour, Chul Kim and 
+ *    Agustin Garcia
+ *
+ */
+public class AssignmentThree
+{
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello, Team 6");
+   }
+
+}
