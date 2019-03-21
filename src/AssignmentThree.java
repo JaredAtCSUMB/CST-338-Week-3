b7513d630deacf40a21905f8029fbbdeda4fd4ce
Tyypos <tyyposofficial@gmail.com>
2019-03-21 06:55:14 -0700

    jared: set up of the Card class

--- src/AssignmentThree.java
+++ src/AssignmentThree.java
@@ -17,10 +17,82 @@ public class AssignmentThree
 
 /*
  * A class that represents a playing card
+ * Values: A, 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K
+ * Suits: clubs, diamonds, hearts, spades
  */
 class Card
 {
-   // empty
+   public enum Suit {clubs, diamonds, hearts, spades};
+   private char value;
+   private boolean errorFlag;
+   private Suit suit;
+   
+   public Card()
+   {
+      this('A', Suit.spades);
+   }
+   
+   // TODO
+   public Card(char value, Suit suit)
+   {
+
+   }
+   
+   // TODO
+   public String toString()
+   {
+      return "";
+   }
+   
+   // TODO
+   public boolean set(char value, Suit suit)
+   {
+      return true;
+   }
+   
+   // TODO
+   public boolean equals(Card card)
+   {
+      return true;
+   }
+   
+   // TODO
+   private boolean isValid(char value, Suit suit)
+   {
+      return true;
+   }
+
+   public Suit getSuit()
+   {
+      return suit;
+   }
+
+   public void setSuit(Suit suit)
+   {
+      this.suit = suit;
+   }
+
+   public char getValue()
+   {
+      return value;
+   }
+
+   public void setValue(char value)
+   {
+      this.value = value;
+   }
+
+   public boolean isErrorFlag()
+   {
+      return errorFlag;
+   }
+
+   public void setErrorFlag(boolean errorFlag)
+   {
+      this.errorFlag = errorFlag;
+   }
+   
+   
 }
 
 /*
